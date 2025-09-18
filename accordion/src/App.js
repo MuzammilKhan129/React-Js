5ceeedb671e8bbe8f3266dde9f3c3af1d0f939ca
moni <muzammilkhan129eb@gmail.com>
2025-09-18 17:55:24 +0500

    reg

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,40 +1,45 @@
 import { useState } from "react";
-import "./styles.css";
+import "./App.css";
 
 const faqs = [
   {
     title: "Where are these chairs assembled?",
-    text:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
   },
   {
     title: "How long do I have to return my chair?",
-    text:
-      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
   },
   {
     title: "Do you ship to countries outside the EU?",
-    text:
-      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
-  }
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
 ];
 
-
-export default function App(){
+export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
-  )
+  );
 }
 
-
-function Accordion() {
+function Accordion(data) {
   return (
     <div>
-      
+      {data.maps((el, i) => (
+        <AcoordionItem title={el.title} text={el.text} />
+      ))}
     </div>
-  )
+  );
 }
 
-
+function AcoordionItem({ num, title, text }) {
+  return (
+    <div className="item">
+      <p className="number">{num}</p>
+      <p className="text">{title}</p>
+      <div className="content-box">{text}</div>
+    </div>
+  );
+}
